87964e4f7d2d1b447e2025efc6501dc527c67e75
Michaël Arnauts <michael.arnauts@destiny.be>
2017-03-09 16:41:52 +0100

    Move environment out of tags to the root

--- tests/unit/DummyRavenClient.php
+++ tests/unit/DummyRavenClient.php
@@ -5,12 +5,16 @@ namespace mito\sentry\tests\unit;
 class DummyRavenClient
 {
     public $tags = [];
+    public $environment;
     public $dsn;
 
     public function __construct($dsn, $options)
     {
         if (isset($options['tags'])) {
             $this->tags = $options['tags'];
+        }
+        if (isset($options['environment'])) {
+            $this->environment = $options['environment'];
         }
         $this->dsn = $dsn;
     }
